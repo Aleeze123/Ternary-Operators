832768bb6b83181980354d829d751837ed221d20
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-07-02 00:58:48 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+/*                                    TERNARY OPERATORS
+In JavaScript and TypeScript, the ternary operator is a concise way to write conditional statements.
+It is often used as a shorthand for simple if...else statements. Here's a definition and explanation of the ternary operator:
+
+Definition:
+
+The ternary operator (condition ? exprIfTrue : exprIfFalse) is a JavaScript/TypeScript operator
+that evaluates a condition and returns one of two expressions depending on whether the condition
+ is true or false.
+
+Explanation:
+
+Syntax: condition ? exprIfTrue : exprIfFalse
+Usage:
+condition: An expression that evaluates to true or false.
+exprIfTrue: The expression to be evaluated if condition is true.
+exprIfFalse: The expression to be evaluated if condition is false.*/
+//Example 1:
+var age = 20;
+var message = (age >= 18) ? 'Adult' : 'Minor';
+console.log(message); // Output: 'Adult'
+//In this example:
+/*
+The condition age >= 18 is evaluated (true in this case).
+If the condition is true, 'Adult' is assigned to message.
+If the condition is false, 'Minor' would be assigned to message.
+ */
+//------------------------------------------------------------------------------------------------------//
+//Example 2
+var userName = "Aleeze";
+var password = 1710;
+userName == "Jannat"
+    ? console.log("You're logged in!")
+    : console.log("Invalid user name or Password!"); // Output Invalid user name or Password
+//------------------------------------------------------------------------------------------------------//
+//Example 3
+var userName1 = "Aleeze";
+var password1 = 1710;
+userName == "Aleeze"
+    ? console.log("You're logged in!")
+    : console.log("Invalid user name or Password!"); // Output: You're logged in!
